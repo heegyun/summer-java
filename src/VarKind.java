16e2a4aa9085c11cbdbf3217100457b02a7c2d95
414-user <414-user@DESKTOP-BR8MIHH>
2018-07-03 16:30:19 +0900

    변수사용

---
+++ src/VarKind.java
@@ -0,0 +1,22 @@
+
+public class VarKind {
+
+	public static void main(String[] args) {
+		
+		char c='¾¾';
+		int i=10;
+		float d =3.14f;
+		boolean b = true;
+		
+		System.out.println("c´Â" + c);
+		System.out.println("i´Â" + i);
+		System.out.println("d´Â" + d);
+		System.out.println("b´Â" + b);
+		
+		
+		
+		
+
+	}
+
+}
